baf452a60e3630fe5df4eb2ca866fbb773cf256a
Ugeshraja <ugeshraja007@gmail.com>
2026-06-24 10:55:40 +0530

    Fix API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,24 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://ai-task-manager-t6xq.onrender.com/api",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-});
+);
 
 export default api;
